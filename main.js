46923b946791adb418fc86030c3ab8ca1a74d7ba
Yuriy <yurenych@gmail.com>
2018-02-01 23:35:40 +0200

    Finished klub.html added slider on klub.html, fixed bugs on ground-golf.html, changed text sizes and line hide on this page.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-$.fatNav();
+$(document).ready(function(){
+	$('.slider').slick( {
+		autoplay: true,
+  		slidesToShow: 2,
+  		slidesToScroll: 1,
+  		speed: 600,
+  		responsive: [
+  			{
+  				breakpoint: 992,
+  				settings: {
+        		arrows: false,
+        		slidesToShow: 1,
+        		speed: 300
+      			}
+      		}	
+  		]
+	});
+});
+
+$(document).ready(function(){
+	$.fatNav();
+});
