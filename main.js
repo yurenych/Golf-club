e896cf9a3724ed3f3be659ee476b56d572e283ee
Yuriy <yurenych@gmail.com>
2018-02-04 11:25:59 +0200

    Changen img path on klub.html

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 	$('.slider').slick( {
 		autoplay: true,
-  		slidesToShow: 2,
-  		slidesToScroll: 1,
-  		speed: 600,
-  		responsive: [
-  			{
-  				breakpoint: 992,
-  				settings: {
-        		arrows: false,
-        		slidesToShow: 1,
-        		speed: 300
-      			}
-      		}	
-  		]
+  	slidesToShow: 2,
+  	slidesToScroll: 1,
+  	speed: 600,
+  	responsive: [
+  		{
+  			breakpoint: 992,
+  			settings: {
+        	arrows: false,
+        	slidesToShow: 1,
+        	speed: 300
+      	}
+      }	
+  	]
 	});
 });
 
