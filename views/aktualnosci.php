6fdf4d62c360b0fa33740f8eebeba2f94e500e59
tverdokhlib <tverdokhlibroman1998@gmail.com>
2018-03-11 22:22:14 +0100

    change html structure in aktualnosci

--- views/aktualnosci.php
+++ views/aktualnosci.php
@@ -27,42 +27,60 @@
   <?php include 'views/inc/navigation.php' ?>
   <!-- end inc navigation menu -->
 
-  <div class="wrapper container-fluid">
-    <div class="headline">Aktualności</div>
-    <div class="news-container">
-
-        <?php $i = 0; ?>
-        <?php foreach ($data['aktualnosci_post'] as $aktualnosci_post) { ?>
-            <div class="news-photo-container row">
-                <h3 class="newsTitle">
-                   <?= $aktualnosci_post['temat'] ?>
-                </h3>
-                <div class="photo-news col-md-3 col-md-offset-2">
-                    <img src="/public/src/img/aktualnosci/<?= $aktualnosci_post['obraz'] ?>">
-                </div>
-                <div class="news-text col-md-5">
-                    <div>
-                       <?= $aktualnosci_post['opis'] ?>
-                    </div>
-                </div>
-
-                <?php if(count($data['aktualnosci_video']) > $i++) { ?>
-                    <?php foreach ($data['aktualnosci_video'] as $aktualnosci_video) { ?>
-                        <div class="videoContainer col-md-2">
-                            <img src="<?= $aktualnosci_video['obraz'] ?>" class="previewVideo">
-                            <div class="videoDescription"><?= $aktualnosci_video['opis'] ?></div>
-                        </div>
-                        <div class="viewVideoContainer">
-                            <img src="/public/src/img/closeButton.png" class="closeButton">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $aktualnosci_video['wideo'] ?>" allowscriptaccess="always" frameborder="0" allow="autoplay; encrypted-media" class="video" allowfullscreen></iframe>
-                        </div>
-                    <?php } ?>
-                <?php } ?>
-            </div>
+<div class="container-fluid">
+  <div class="row mainContainer">
+    <div class="col-md-2 videoWrapper">
+      <?php if(count($data['aktualnosci_video']) > $i++) { ?>
+        <?php foreach ($data['aktualnosci_video'] as $aktualnosci_video) { ?>
+          <div class="videoContainer">
+              <img src="<?= $aktualnosci_video['obraz'] ?>" class="previewVideo">
+              <div class="videoDescription"><?= $aktualnosci_video['opis'] ?></div>
+          </div>
+          <div class="viewVideoContainer">
+              <img src="/public/src/img/closeButton.png" class="closeButton">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $aktualnosci_video['wideo'] ?>" allowscriptaccess="always" frameborder="0" allow="autoplay; encrypted-media" class="video" allowfullscreen></iframe>
+          </div>
         <?php } ?>
-
+      <?php } ?>
     </div>
+    <div class="wrapper col-md-8">
+      <div class="headline">Aktualności</div>
+      <div class="news-container">
+          <?php $i = 0; ?>
+          <?php foreach ($data['aktualnosci_post'] as $aktualnosci_post) { ?>
+              <div class="news-photo-container row">
+                  <h3 class="newsTitle">
+                     <?= $aktualnosci_post['temat'] ?>
+                  </h3>
+                  <div class="photo-news col-md-3">
+                      <img src="/public/src/img/aktualnosci/<?= $aktualnosci_post['obraz'] ?>">
+                  </div>
+                  <div class="news-text col-md-9">
+                      <div>
+                         <?= $aktualnosci_post['opis'] ?>
+                      </div>
+                  </div>
+              </div>
+          <?php } ?>
+
+      </div>
+    </div>
+    <div class="col-md-2 videoWrapper">
+      <?php if(count($data['aktualnosci_video']) > $i++) { ?>
+        <?php foreach ($data['aktualnosci_video'] as $aktualnosci_video) { ?>
+          <div class="videoContainer ">
+              <img src="<?= $aktualnosci_video['obraz'] ?>" class="previewVideo">
+              <div class="videoDescription"><?= $aktualnosci_video['opis'] ?></div>
+          </div>
+          <div class="viewVideoContainer">
+              <img src="/public/src/img/closeButton.png" class="closeButton">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $aktualnosci_video['wideo'] ?>" allowscriptaccess="always" frameborder="0" allow="autoplay; encrypted-media" class="video" allowfullscreen></iframe>
+          </div>
+        <?php } ?>
+      <?php } ?>
+      </div>
   </div>
+</div>
 
   <!-- inc footer -->
   <?php include 'views/inc/footer.php' ?>
