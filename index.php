62a904fe4cfb07ea23ffa204aaa773f684807469
Vlad Trishch <vlad.trishch@gmail.com>
2018-04-19 22:06:10 +0300

    Added proper php debugging tool

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ if(!isset($_SESSION)){
     session_start();
 }
 
+function var_log( $object=null ){
+  ob_start();                    // start buffer capture
+  var_dump( $object );           // dump the values
+  $contents = ob_get_contents(); // put the buffer into a variable
+  ob_end_clean();                // end capture
+  error_log( $contents );        // log contents of the result of var_dump( $object )
+}
+
 // Include config.
 require_once("config.php");
 
